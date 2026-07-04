328d5bf100a2aec858487c31af9b78b42cceb509
yashwanthbgowda9035 <yashwanthb2007@gmail.com>
2026-07-04 14:10:28 +0530

    Fix app startup for Render

--- app.py
+++ app.py
@@ -767,5 +767,5 @@ def logout():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    init_db()
-app.run(host="0.0.0.0", port=5000, debug=True)
+     init_db()
+     app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
